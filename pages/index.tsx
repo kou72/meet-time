eb44bb95a7d6ae03e92060d5e22b3e8dd0aa4a66
kou72 <genpokopokopoko@gmail.com>
2023-07-31 00:48:06 +0900

    _meta.pngファイルが変更されているが、バイナリファイルのため変更内容は分析できない_
    _document.tsx
    - og:titleの値を"Meet Time"から"MeetTime"に変更
    
    index.tsx
    - titleタグの値を"Meet Time"から"MeetTime"に変更
    - h1タグのテキストを"Meet Time"から"MeetTime"に変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,9 +96,9 @@ export default function MyCalendar() {
 
   return (
     <div className="h-screen bg-slate-200">
-      <title>Meet Time</title>
+      <title>MeetTime</title>
       <div className="h-12 bg-gray-700 flex items-center justify-center">
-        <h1 className="font-bold text-center text-white">Meet Time</h1>
+        <h1 className="font-bold text-center text-white">MeetTime</h1>
       </div>
       <div className="flex mt-8 mx-12 h-4/5">
         <div className="flex-grow">
