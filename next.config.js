a8d05f8cb09ee436034e32ce06ba39f11a7a94cd
kou72 <genpokopokopoko@gmail.com>
2023-07-30 22:41:05 +0900

    next.config.jsファイルのbasePathの設定を修正
    next.config.js
    - basePathの値をprocess.env.GITHUB_ACTIONSがtrueの場合は"/"、それ以外の場合は空文字に変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.GITHUB_ACTIONS && "/",
+  basePath: process.env.GITHUB_ACTIONS && "",
   reactStrictMode: false,
 };
 
