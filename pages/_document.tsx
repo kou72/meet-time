6976e4b47fa63525538a4d5300d509d37ed4f51d
kou72 <genpokopokopoko@gmail.com>
2023-07-30 21:32:38 +0900

    _document.tsxファイルとmeta.pngファイルの変更点を分析
    _document.tsx
    - metaタグにog:titleとog:descriptionを追加
    
    meta.png
    - バイナリファイルの変更があるため、具体的な変更内容は分からない

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,10 +10,12 @@ export default function Document() {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>📅</text></svg>"
         />
+        <meta property="og:title" content="Meet Time" />
         <meta
           property="og:image"
           content="https://raw.githubusercontent.com/kou72/meet-time/main/public/meta.png"
         />
+        <meta property="og:description" content="スケジュールを簡単に共有" />
       </Head>
       <body>
         <Main />
