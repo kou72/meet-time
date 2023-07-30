69ebc83fc52858f6dec74ab6d5131c190865bb35
kou72 <genpokopokopoko@gmail.com>
2023-07-30 16:55:16 +0900

    pages/index.tsxファイルの修正内容を解析すると以下の通りです。
    pages/index.tsx
    - "@fullcalendar/timegrid"のインポートを追加
    - timeGridPluginをpluginsに追加
    - initialViewを"timeGridWeek"に変更
    - eventsプロパティを削除
    - コメント行を削除
    
    以上がpages/index.tsxファイルの変更内容です。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
-import timeGridPlugin from "@fullcalendar/timegrid"; // Use timeGrid plugin instead of dayGrid
+import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 export default function MyCalendar() {
@@ -12,11 +12,9 @@ export default function MyCalendar() {
     endStr: any;
     allDay: any;
   }) => {
-    // clear date selection
     let calendarApi = selectInfo.view.calendar;
     calendarApi.unselect();
 
-    // Add event to calendar without prompt
     calendarApi.addEvent({
       id: createEventId(),
       start: selectInfo.startStr,
@@ -27,17 +25,15 @@ export default function MyCalendar() {
 
   return (
     <FullCalendar
-      plugins={[timeGridPlugin, interactionPlugin]} // Use timeGridPlugin here
-      initialView="timeGridWeek" // Change to 'timeGridWeek' for week view
+      plugins={[timeGridPlugin, interactionPlugin]}
+      initialView="timeGridWeek"
       selectable={true}
       select={handleDateSelect}
       ref={calendarRef}
-      events={[]}
     />
   );
 }
 
-// for generating unique ids
 let eventGuid = 0;
 function createEventId() {
   return String(eventGuid++);
